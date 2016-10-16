588b4dd1c0d5900bd319c7b46c12b54a53f59190
christianscholz <christianscholz@christians-mbp.fritz.box>
2016-10-16 18:04:55 +0200

    Initial Commit

---
+++ HelloChristian.java
@@ -0,0 +1,11 @@
+
+public class HelloChristian {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a test and we know this !");
+		System.out.println("Water is black !");
+
+	}
+
+}
